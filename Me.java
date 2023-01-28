fe5b38ada73d7b0e1ff895cc018de0ad09f94aff
anthonyabj <anthony.aboujaoude@lau.edu>
2023-01-28 15:29:22 +0200

    My year of study added

--- Me.java
+++ Me.java
@@ -2,6 +2,7 @@ public class Me
 {
 	public static void main(String[] args){
 		System.out.println("Anthony Abou Jaoude");
-		System.out.println("Computer Engineering");
+		System.out.println("Computer Engineering");\
+		System.out.println("2022")
 	}
 }
